3d8b2e7a8f710a58b474de2cbfac06856274b641
lepon01 <leondrolio@gmail.com>
2017-06-10 14:29:26 +0100

    Setinterval "just in case" am i rite

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 let discordelements = {};
 
-//Get the largest element that hasn't been fucked up yet
-discordelements["flex-vertical channels-wrap"] = document.getElementsByClassName("flex-vertical channels-wrap")[0]
+setInterval(()=>{
+	//Get the largest element that hasn't been fucked up yet
+	discordelements["flex-vertical channels-wrap"] = document.getElementsByClassName("flex-vertical channels-wrap")[0]
 
-//Get the channels column
-discordelements["flex-vertical flex-spacer guild-channels-wrapper"] = discordelements["flex-vertical channels-wrap"].childNodes[0]
-discordelements["guild-header"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[0]
-discordelements["channel-list-top"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[1]
-discordelements["scroller-wrap fade dark"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[2]
-discordelements["scroller guild-channels"] = discordelements["scroller-wrap fade dark"].childNodes[0]
+	//Get the channels column
+	discordelements["flex-vertical flex-spacer guild-channels-wrapper"] = discordelements["flex-vertical channels-wrap"].childNodes[0]
+	discordelements["guild-header"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[0]
+	discordelements["channel-list-top"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[1]
+	discordelements["scroller-wrap fade dark"] = discordelements["flex-vertical flex-spacer guild-channels-wrapper"].childNodes[2]
+	discordelements["scroller guild-channels"] = discordelements["scroller-wrap fade dark"].childNodes[0]
 
-//Fix each channel box
-discordelements["channel channel-text"] = Array.from(discordelements["scroller guild-channels"].childNodes).filter((element)=>element.hasAttribute("draggable"))
+	//Fix each channel box
+	discordelements["channel channel-text"] = Array.from(discordelements["scroller guild-channels"].childNodes).filter((element)=>element.hasAttribute("draggable"))
 
-//Get the account box
-discordelements["account"] = discordelements["flex-vertical channels-wrap"].childNodes[2]
-discordelements["avatar-small stop-animation"] = discordelements["account"].childNodes[0]
-discordelements["account-details"] = discordelements["account"].childNodes[1]
-//discordelements["btn-group"] = discordelements["account"].childNodes[2]
+	//Get the account box
+	discordelements["account"] = discordelements["flex-vertical channels-wrap"].childNodes[2]
+	discordelements["avatar-small stop-animation"] = discordelements["account"].childNodes[0]
+	discordelements["account-details"] = discordelements["account"].childNodes[1]
+	//discordelements["btn-group"] = discordelements["account"].childNodes[2]
 
-Object.keys(discordelements).forEach((key) => {
-	if (Array.isArray(discordelements[key])) {
-		discordelements[key].forEach((element)=>{
-			element.className = key;
-		});
-	} else if(typeof discordelements[key] == "object") {
-		discordelements[key].className = key;
-	}
-})
+	Object.keys(discordelements).forEach((key) => {
+		if (Array.isArray(discordelements[key])) {
+			discordelements[key].forEach((element)=>{
+				element.className = key;
+			});
+		} else if(typeof discordelements[key] == "object") {
+			discordelements[key].className = key;
+		}
+	})
+}, 500)
+
